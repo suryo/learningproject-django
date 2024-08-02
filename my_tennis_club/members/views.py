708dd48c855fe8e2d6675d428081b2cb4dfcd25c
suryo <suryoatm@gmail.com>
2024-08-02 13:07:46 +0700

    view template model migration

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
 # Create your views here.
+
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
